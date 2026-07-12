5daee1112f72e351fa33e9d7bd0703022752a8b3
zhangzhewen <nsync_zzw@163.com>
2026-07-12 21:48:22 +0800

    调整Spring环境配置

--- src/test/java/com/saasbase/api/ApiPathPartitionTest.java
+++ src/test/java/com/saasbase/api/ApiPathPartitionTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = SaaSBaseApplication.class)
-@ActiveProfiles("test")
+@ActiveProfiles("staging")
 class ApiPathPartitionTest {
 
     @Autowired
