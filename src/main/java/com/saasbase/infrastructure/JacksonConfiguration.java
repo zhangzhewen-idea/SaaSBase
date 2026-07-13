bbfc20443ef868b7c86954a53865115ea1fe3d15
zhangzhewen <nsync_zzw@163.com>
2026-07-13 09:33:03 +0800

    修正对象映射器配置

--- src/main/java/com/saasbase/infrastructure/JacksonConfiguration.java
+++ src/main/java/com/saasbase/infrastructure/JacksonConfiguration.java
@@ -3,12 +3,18 @@ package com.saasbase.infrastructure;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfiguration {
 
     @Bean
-    ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
+    }
+
+    @Bean
+    ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.build();
     }
 }
