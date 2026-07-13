a97a62c51597b58d13c36fa663ca427cdd8fef39
zhangzhewen <nsync_zzw@163.com>
2026-07-13 09:26:01 +0800

    补充对象映射器配置

---
+++ src/main/java/com/saasbase/infrastructure/JacksonConfiguration.java
@@ -0,0 +1,14 @@
+package com.saasbase.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
